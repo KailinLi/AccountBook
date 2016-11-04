b7f5f371bb2728642b6e286cb380fb380cab9b8d
李恺林 <likailin@likailindeair.lan>
2016-11-04 21:26:59 +0800

    Initial Commit

---
+++ AccountBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AccountBook
+//
+//  Created by 李恺林 on 2016/11/4.
+//  Copyright © 2016年 李恺林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
